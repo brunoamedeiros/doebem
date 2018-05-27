3bf7d66177ca7e1c275db093212ea5c459686feb
Jhonatas <jhonatas.maurilio@hotmail.com>
2018-05-27 12:56:09 -0300

    Visualização do mapa
    Ajustado visualização do mapa, na tela de visualziação de informações da istituição.

--- views/instituicao/view.php
+++ views/instituicao/view.php
@@ -86,8 +86,8 @@ $this->title = 'Visualizar dados';
         <?php endif;?>
     </div>
 
-    <div class="infos-local col-lg-6 col-md-6 col-sm-12 float-right">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.9740252873767!2d-49.02335288492086!3d-28.480328882478148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x952142f303127973%3A0x88b99e946a08dd5b!2sUnisul+-+Universidade+do+Sul+de+Santa+Catarina!5e0!3m2!1spt-BR!2sbr!4v1527435358595" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="infos-local col-lg-6 col-md-6 col-sm-12 float-right p0">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.9740252873767!2d-49.02335288492086!3d-28.480328882478148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x952142f303127973%3A0x88b99e946a08dd5b!2sUnisul+-+Universidade+do+Sul+de+Santa+Catarina!5e0!3m2!1spt-BR!2sbr!4v1527435358595"  width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
 
     <div class="clear-both"></div>
