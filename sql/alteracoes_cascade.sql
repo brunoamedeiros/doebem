2dfc1133c4aa4bfa07436552e86d3868eb4ab614
Anderson Cardoso Gaspar <anderson.gaspar@unisul.br>
2018-06-05 21:35:24 -0300

    Anderson
    Scripts para deixar nulo alguns campos.

--- sql/alteracoes_cascade.sql
+++ sql/alteracoes_cascade.sql
@@ -4,3 +4,6 @@ ALTER TABLE `contribuicao` DROP FOREIGN KEY `contribuicao_ibfk_3`;
 ALTER TABLE `contribuicao` ADD  CONSTRAINT `contribuicao_ibfk_3` FOREIGN KEY (`id_contribuinte`) REFERENCES `contribuinte`(`id_contribuinte`) ON DELETE CASCADE ON UPDATE RESTRICT;
 ALTER TABLE `resultado` DROP FOREIGN KEY `resultado_ibfk_1`;
 ALTER TABLE `resultado` ADD  CONSTRAINT `resultado_ibfk_1` FOREIGN KEY (`id_doacao`) REFERENCES `doacao`(`id_doacao`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE `instituicao` CHANGE `perfil` `perfil` INT(11) NULL;
+ALTER TABLE `doacao` CHANGE `data_publicacao` `data_publicacao` DATE NULL;
+ALTER TABLE `item` CHANGE `imagem` `imagem` VARCHAR(255)  NULL;
